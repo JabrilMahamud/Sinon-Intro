a8eef9e44d00c4644a2278fae921bef7fb3a85fe
JabrilMahamud <91251262+JabrilMahamud@users.noreply.github.com>
2022-01-19 14:36:47 +0000

    after lunch

--- test/notification_service_test.js
+++ test/notification_service_test.js
@@ -2,6 +2,7 @@ const { expect } = require('chai');
 const sinon = require('sinon');
 const notifications = require('../src/notification_service');
 const tweeter = require('../src/tweet_service');
+const emailer = require(`../src/email_service`)
 
 describe('notification service', function () {
     const userProfile = {
@@ -10,12 +11,23 @@ describe('notification service', function () {
     };
 
     it('triggers email to be sent', function () {
+        const createEmailer = sinon.stub(emailer, 'createEmailer');
+        const fakeEmailer = {sendEmail: sinon.spy()};
+        createEmailer.returns(fakeEmailer);
+
         const notificationService = notifications.buildNotificationService();
-        notificationService.sendNotifications(userProfile, 'Hello');
+        const message = 'Hello';
+        notificationService.sendNotifications(userProfile, message);
+        expect(fakeEmailer.sendEmail.calledOnce).to.be.true;
+        expect(fakeEmailer.sendEmail.getCall(0).args[0]).to.equal(userProfile.emailAddress);
+        expect(fakeEmailer.sendEmail.getCall(0).args[1]).to.equal(message);
+
         // Oops, did we just email somebody?
         // How do we check if anything happened?
     });
-
+        this.afterEach(function(){
+            sinon.restore()
+        })
     it('triggers tweet to be sent', function () {
         const createTweeter = sinon.stub(tweeter, 'createTweeter');
         const fakeTweeter = {sendTweet: sinon.spy()};
